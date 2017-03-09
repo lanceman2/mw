05d260c5beaf2dddabfbe1c6150ecf79af0e4b1e
Lance Arsenault <lanceman@vt.edu>
2017-03-09 17:45:02 -0500

    saving work

---
+++ public/mw/example.js
@@ -0,0 +1,3 @@
+mw_addActor('actor/plane.x3d');
+mw_addActor('actor/simpleExamples/move.x3d');
+mw_addActor('actor/simpleExamples/move.x3d');
--- public/mw/mw_client.js
+++ public/mw/mw_client.js
@@ -1,5 +1,3 @@
-var _mw_scene = false;
-
 function mw_fail() {
 
     var text = "Something has gone wrong";
@@ -29,38 +27,50 @@ function _mw_getElementById(id) {
     return element;
 }
 
-// returns an array of all nodes with attribute
-function _mw_findAttributes(node, attribute, func = false) {
+
+// Searches node and all its' children and
+// returns an array of returnFunc() things that testFunc() was true for.
+// There are default testFunc and returnFunc functions.
+function _mw_findNodes(node, param,
+        returnFunc = function(node, param) {
+            return node.getAttribute(param);
+        },
+        testFunc = function(node, param) { 
+            return node.hasAttribute && node.hasAttribute(param);
+        }) {
+
+    if(node === undefined || !node) return [];
 
     var ret = [];
 
-    if(node === undefined || !node) return false;
-
-    //console.log('node = ' + node.innerHTML);
-
-    if(node.hasAttribute && node.hasAttribute(attribute)) {
-        if(func)
-            ret = [func(node)];
-        else
-            ret = [node.getAttribute(attribute)];
-    }
+    if(testFunc(node, param))
+        ret = [returnFunc(node, param)];
 
     for(node = node.firstChild; node !== undefined && node ;
             node = node.nextSibling) {
-        var r = _mw_findAttributes(node, attribute, func);
-        if(r.length > 0)
-            ret = ret.concat(r);
+        var r = _mw_findNodes(node, param, returnFunc, testFunc);
+        if(r.length > 0) ret = ret.concat(r);
     }
 
     return ret;
 }
 
 
+// Searches node and all its' children and
+// returns an array of all nodes with attribute from node and all
+// its' children.
+function _mw_findAttributes(node, attribute) {
+
+    return _mw_findNodes(node, attribute);
+}
+
+
+// actorCalls is an array of strings.
 function _mw_runFunctions(actorCalls)
 {
     actorCalls.forEach(
         function(call) {
-            console.log('MW CALLING: ' + call.call + '(' +
+            console.log('MW Calling: ' + call.call + '(' +
                     call.node + ')');
             window[call.call](call.node);
         }
@@ -68,28 +78,51 @@ function _mw_runFunctions(actorCalls)
 }
 
 
-function _mw_addScripts(actorScriptUrls, actorCalls) {
+function _mw_addScript(src, onloadFunc = null) {
 
-    var count = actorScriptUrls.length;
-    actorScriptUrls.forEach(function(src) {
-        
-        var script = document.createElement('script');
-        document.head.appendChild(script);
-        script.src = src;
-        script.onload = function() {
+    console.log('MW Adding Script src= ' + src);
+    var script = document.createElement('script');
+    document.head.appendChild(script);
+    script.src = src;
+    script.onload = onloadFunc;
+}
+
+
+function _mw_addCss(href, onloadFunc = null) {
+
+    console.log('MW Adding CSS href= ' + href);
+    var link = document.createElement('link');
+    document.head.appendChild(link);
+    link.setAttribute("rel", "stylesheet");
+    link.setAttribute("type", "text/css");
+    link.setAttribute("href", href)
+    link.onload = onloadFunc;
+}
+
+
+// actorScriptUrls and actorCalls are arrays of strings.
+function _mw_addScripts(actorScriptUrls, actorCalls = null) {
+
+    if(actorCalls && actorCalls.length) {
+        var count = actorScriptUrls.length;
+        var check = function() {
             --count;
-            console.log('MW LOADED script: ' + src);
-            actorScriptUrls.pop();
-            if(count === 0) {
+            if(count === 0)
                 // We call after all scripts are loaded:
                 _mw_runFunctions(actorCalls);
-            }
         };
+
+    } else
+        var check = null;
+
+    actorScriptUrls.forEach( function(src) {
+
+        _mw_addScript(src, check);
     });
 }
 
 
-function mw_addActor(url) {
+function _mw_addX3dActor(url) {
 
     var group = document.createElement('group');
     _mw_assert(group);
@@ -97,51 +130,215 @@ function mw_addActor(url) {
     _mw_assert(inline);
     inline.setAttribute("namespacename", url);
     inline.setAttribute("url", url);
-    
+
     inline.onload = function() {
 
-        var actorScripts = _mw_findAttributes(this, 'data-mw_script');
-        var actorScriptUrls = [];
-        var actorCalls = _mw_findAttributes(this, 'data-mw_call',
-                function(node) {
-                    return { node: node , call: node.getAttribute('data-mw_call') };
+        var dir = inline.url.replace(/[^\/]*$/, '');
+        var actorScripts = _mw_findNodes(this, 'data-mw_script',
+                function(node, attribute) {
+                    var src = node.getAttribute(attribute);
+                    if(src.substr(0,1) !== '/') {
+                        return  dir + src;
+                    }
+                    else
+                        return src;
+                }
+        );
+        var actorCalls = _mw_findNodes(this, 'data-mw_call',
+                function(node, attribute) {
+                    return { node: node , call: node.getAttribute(attribute) };
                 }
         );
 
-        var dir = inline.url.replace(/[^\/]*$/, '');
-
-        actorScripts.forEach(function(src) {
-            // TODO: make this handle a general URL
-            // not just a path.
-            if(src.substr(0,1) !== '/')
-                actorScriptUrls.push(dir + src);
-            else
-                actorScriptUrls.push(src);
-        });
-
-        _mw_addScripts(actorScriptUrls, actorCalls);
-    }
+        // if the xd3 file had data-mw_script and/or data-mw_call
+        // attributes we load the scripts and run the "mw_call" functions.
+        _mw_addScripts(actorScripts, actorCalls);
+    };
 
     group.appendChild(inline);
-    _mw_getElementById('mw_scene').appendChild(group);
+
+    if(_mw.scene === undefined) {
+        var scenes = _mw_findNodes(
+                document.getElementsByTagName("BODY")[0], 'SCENE',
+            function (node, nodeName) {
+                return node; // what to return in an array
+            },
+            function (node, nodeName) {
+                // The test function
+                return node.nodeName === nodeName;
+            }
+        );
+        _mw_assert(scenes.length === 1, 'scenes=' + scenes);
+        _mw.scene = scenes[0];
+    }
+
+    _mw.scene.appendChild(group);
 }
+
+function mw_addActor(url) {
+
+    console.log('MW Adding Actor: ' + url);
+
+    // TODO: consider adding a query part to the URL
+
+    var suffix = url.replace(/^.*\./g, '').toLowerCase();
+
+    switch (suffix) {
+        case 'x3d':
+            _mw_addX3dActor(url);
+            return;
+        case 'js':
+            _mw_addScript(url);
+            return;
+        case 'css':
+            _mw_addCss(url);
+            return;
+        default:
+            console.log('MW Unknown Actor type: ' + url);
+    }
+}
+
+
+// (TODO) This must verify the that this subscription creation is
+// permitted from the server.  Command "Creates Subscriptions" to the
+// server.  this is the socket.
+function _mw_createSubscription(decoder) {
+    
+    _mw_assert(this !== window);
+
+    this.emit('subscription', decoder /*TODO*/);
+}
+
+function _mw_subscribe(obj) {
+
+    _mw_assert(this !== window);
+
+    // TODO:
+    
+    console.log('subscribe(' + obj + ')');
+}
+
+
+// update from this client to the server writing subscription channels
+// from this client to the server.
+// this is the socket.  Subscribed clients will have the decoder already
+// so we do not waist bandwidth resending it again and again.
+// obj should be minimum data needed for client to use.
+function _mw_emitUpdate(obj/*array of objects or single object*/) {
+
+    _mw_assert(this !== window);
+
+    this.emit('update', obj/*TODO*/);
+}
+
+
+// initiate(socket) called in connect callback.
+//
+function mw_init(userInit = function(mw) {
+        },
+        subscription = function(mw, obj) {
+        }) {
+
+    // Change 'http' to 'ws' and 'https' to 'wss'
+    var socketUrl = location.href.replace(/^http/, 'ws');
+
+    // the mw object inherits the socket.io object
+    // the mw object is the socket.io object
+    var socket = new io.connect('');
+
+    socket.on('connect', function(event) {
+        console.log('MW Connected Socket.IO to ' + socketUrl);
+        socket.createSubscription = _mw_createSubscription;
+        socket.emitUpdate = _mw_emitUpdate;
+        socket.subscribe = _mw_subscribe;
+        // The users' callback function get to use the mw object
+        // that is the socket.
+        userInit(socket);
+        socket.emit('initiate', 'default');
+    });
+    socket.on('initiate', function(data0) {
+        console.log('MW Recieved Socket.IO initiate message ' +
+                data0);
+
+        // TODO: find the currently available subscriptions here.
+
+        socket.createSubscription = _mw_createSubscription;
+        socket.emitSubscription = _mw_emitSubscription;
+    });
+
+    // Incoming socket.on  Commands 
+    //
+    //   update:  Get subscribed data in an array of subscriptions
+    //            [] array of channels
+    //
+    //   subscription:  Pops up when available subscriptions change
+    //                  [] array of channels that we may subscribe to
+    //
+    //
+    // Outgoing socket.emit Commands
+    //
+    //   update:  Put subscription data from this client as a source
+    //            [] array of channels or single
+    //
+    //   subscription:  Creates subscriptions
+    //                  [] array of channels that we would like to create
+    //                  or single
+    //
+    //
+
+    socket.on('update', function(data) {
+        // TODO: Code to receive subscribed data:
+        console.log('MW Recieved Socket.IO update message ' +
+                data);
+    });
+
+    socket.on('subscription', function(data) {
+        // TODO: Code to receive possible subscriptions:
+        console.log('MW Recieved Socket.IO subscriptions ' +
+                data);
+    });
+
+    socket.on('disconnect', function() {				
+        socket.disconnect();
+        socket.removeAllListeners(); // Now it should not reconnect.
+        console.log('MW Socket.IO server at ' + location.host + ' disconnected');
+        delete _mw.socket;
+        _mw.socket = null;
+    });
+}
+
+
+
+function _mw_bodyPreload() {
+
+    // This stuff is required to be loaded before the page onload is called,
+    // so we call it now.
+    mw_addActor('x3dom/x3dom.css');
+    mw_addActor('x3dom/x3dom.js');
+    mw_addActor('/socket.io/socket.io.js');
+    mw_addActor('mw_client_default.css');
+}
+
+
+
+function _mw_up
 
 
 // Called from body onload event.
 function mw_main() {
 
-    // Change 'http' to 'ws' and 'https' to 'wss'
-    var socketUrl = location.href.replace(/^http/, 'ws');
-    socket = new WebSocket(socketUrl);
-    socket.onopen = function(event) {
-        console.log('MW Connected webSocket to ' + socketUrl);
-    }
-    socket.onmessage = function(event) {
-        console.log('MW Client received the message ' + event.data);
-    }
-    socket.onclose = function(event) {
-        console.log('MW Client webSocket to ' + socketUrl + ' closed');
-    }
-
-    mw_addActor('actor/simpleExamples/move.x3d');
+    mw_init( /*init()*/function(mw) {
+            mw.createSubscription('foo');
+            mw_addActor('example.js');
+        },
+        function(mw, subscription) {
+            mw.subscribe(subscription);
+        }
+    );
 }
+
+
+// Call this now before the body onload function so that it's all
+// loaded before the body onload function mw_main() gets called.
+// We could use callbacks to do this, but this is way less coding.
+_mw_bodyPreload();
